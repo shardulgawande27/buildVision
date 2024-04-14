723497098afb7cc6a80e1ed93e59a989bd674365
shardulgawande27 <shardulgawande27@gmail.com>
2024-04-14 18:11:36 +0530

    registration

--- backend/src/controllers/users/userController.ts
+++ backend/src/controllers/users/userController.ts
@@ -1,11 +1,11 @@
 import { RequestWithProfile } from '@/src/types';
 import { Request, Response } from 'express';
 import { statusType as statusCode} from "@/src/enum/statusTypes";
-const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 import { sendResponse } from '@/src/services/ResponseService';
 // import { loginUser, registerUser } from './userJoiModel';
 import knex from '../../db';
+import bcrypt from "bcrypt";
 import { isNativeError } from 'util/types';
 
 export async function register(req: Request, res: Response) {
@@ -20,16 +20,35 @@ export async function register(req: Request, res: Response) {
       case 'consultant':
         const { m_consultant_name, m_consultant_address, m_consultant_username, m_consultant_password} = req.body;
         console.log({ m_consultant_name, m_consultant_address, m_consultant_username, m_consultant_password}, "data >>>>>>>>>>>>>")
-        await knex('m_consultant').insert({ m_consultant_name, m_consultant_address, m_consultant_username, m_consultant_password});
+        const hashedPassword = await bcrypt.hash(m_consultant_password, 10)
+        await knex('m_consultant').insert({ 
+          m_consultant_name, 
+          m_consultant_address, 
+          m_consultant_username, 
+          m_consultant_password:hashedPassword});
         break;
-      case 'developer':
-        const {developer_name	,developer_address	,developer_username	,developer_email	,developer_password	} =  req.body;
-        await knex('m_developer').insert({developer_name	,developer_address	,developer_username	,developer_email	,developer_password	});
-        break;
-      case 'scoiety':
-        const {m_society_name	,m_society_address,	m_society_email,	m_society_password,	m_society_username} = req.body;
-        await knex('m_society').insert({m_society_name	,m_society_address,	m_society_email,	m_society_password,	m_society_username});
-        break;
+        case 'developer':
+          const { m_developer_name, m_developer_address, m_developer_username, m_developer_email, m_developer_password } = req.body;
+          const hashedDeveloperPassword = await bcrypt.hash(m_developer_password, 10);
+          await knex('m_developer').insert({
+              m_developer_name,
+              m_developer_username,
+              m_developer_email,
+              m_developer_address,
+              m_developer_password: hashedDeveloperPassword
+          });
+          break;
+      case 'society':
+          const { m_society_name, m_society_address, m_society_email, m_society_password, m_society_username } = req.body;
+          const hashedSocietyPassword = await bcrypt.hash(m_society_password, 10);
+          await knex('m_society').insert({
+              m_society_name,
+              m_society_address,
+              m_society_email,
+              m_society_password: hashedSocietyPassword,
+              m_society_username
+          });
+          break;
       default:
         return res.status(400).json({ error: 'Invalid role.' });
       
