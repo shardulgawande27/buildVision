d2f3a4d0347e78c28c863b405723a0fb551fb802
shardulgawande27 <shardulgawande27@gmail.com>
2024-04-16 00:07:47 +0530

    login api

--- backend/src/controllers/users/userController.ts
+++ backend/src/controllers/users/userController.ts
@@ -1,12 +1,16 @@
+require("dotenv").config();
 import { RequestWithProfile } from '@/src/types';
 import { Request, Response } from 'express';
 import { statusType as statusCode} from "@/src/enum/statusTypes";
-const jwt = require("jsonwebtoken");
+import jwt from 'jsonwebtoken';
 import { sendResponse } from '@/src/services/ResponseService';
 // import { loginUser, registerUser } from './userJoiModel';
 import knex from '../../db';
 import bcrypt from "bcrypt";
 import { isNativeError } from 'util/types';
+import { error } from 'console';
+import { config } from 'process';
+
 
 export async function register(req: Request, res: Response) {
   try {
@@ -58,5 +62,32 @@ export async function register(req: Request, res: Response) {
     res.status(200).json({message : `${role} is saved`})
   } catch (error: any) {
     sendResponse(res, statusCode.DB_ERROR, error.message, null);
+  }
+}
+
+export async function login(req: Request, res: Response){
+   console.log(process.env.SECRET)
+  try {
+    
+    const role = req.body.role;
+
+    switch (role) {
+      case 'consultant':
+        const{m_developer_email, m_consultant_password} = req.body;
+        const consultant = await knex('m_consultant').where({m_developer_email}).first();
+        if(!consultant || !await bcrypt.compare(m_consultant_password, consultant.m_consultant_password)){
+          return res.status(401).json({error:"Invalid email or password"});
+        }
+        const key = process.env.SECRET;
+        const token = jwt.sign({userId: consultant.m_consultant_id, role:'consultant'},key, { expiresIn: '1h' })
+        return res.status(200).json({ token });
+        break;
+    
+      default:
+        break;
+    }
+
+  } catch (error:any) {
+    
   }
 }
