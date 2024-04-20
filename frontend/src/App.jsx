ba09fa98c7b8e9b3625a98806b2d6fd912b9925c
shardulgawande27 <shardulgawande27@gmail.com>
2024-04-20 11:10:58 +0530

    home main

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import RouteConfig from "./pages/route/RouteConfig";
+import "./App.css";
 const App = () => {
   return (
     <Router>
