53638cd35a285d3cafcab1c5d91b16c471f05f20
shardulgawande27 <shardulgawande27@gmail.com>
2024-04-20 10:13:41 +0530

    home

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 const usersRoute = require("@/src/controllers/users")
 const pmcRoute = require("@/src/controllers/pmc")
+const homeRoute = require('@/src/controllers/home')
 
 router.use("/users", usersRoute);
 // router.use("/pmc", pmcRoute);
+router.use("/home", homeRoute)
 
 
 module.exports = router;
