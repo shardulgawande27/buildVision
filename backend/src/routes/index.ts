8c68273648fca01cbf820d191f2fb9ee2a4dd0a9
Harhsal Mahajan <harshal.mahajan@enpointe.io>
2024-04-27 00:47:44 +0530

    image upload

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -3,11 +3,13 @@ const usersRoute = require("@/src/controllers/users")
 const pmcRoute = require("@/src/controllers/pmc")
 const homeRoute = require('@/src/controllers/home')
 const projectRoute = require('@/src/controllers/projects')
+const uploadRoute = require('@/src/controllers/upload')
 
 router.use("/users", usersRoute);
 // router.use("/pmc", pmcRoute);
 router.use("/home", homeRoute)
 router.use("/project", projectRoute)
+router.use("/upload", uploadRoute)
 
 
 module.exports = router;
