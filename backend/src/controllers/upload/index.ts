a7b4644121feb66f53c1dff9c629fbb4c09fb045
Harhsal Mahajan <harshal.mahajan@enpointe.io>
2024-05-04 11:42:28 +0530

    fix file upload

--- backend/src/controllers/upload/index.ts
+++ backend/src/controllers/upload/index.ts
@@ -7,7 +7,17 @@ const router: IRouter = express.Router();
 
 const uploadDir = path.resolve(__dirname, 'uploads/'); // Absolute path to the uploads directory
 
-const upload = multer({ dest: uploadDir });
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, uploadDir); // Path to save uploaded files
+  },
+  filename: (req, file, cb) => {
+    const ext = path.extname(file.originalname);
+    cb(null, `${file.fieldname}-${Date.now()}${ext}`); // Filename format
+  }
+});
+const upload = multer({ storage: storage });
+
 
 
 router.post('/uploadImage', upload.single('image'), async (req, res) => {
--- frontend/src/api/uploadService.js
+++ frontend/src/api/uploadService.js
@@ -1,8 +1,15 @@
 import Api from "./api.js";
 
+const configHeaders = {
+  headers: {
+      'content-type': 'multipart/form-data',
+      'Access-Control-Allow-Origin': '*'
+  }
+};
+
 export default {
   uploadImage(payload) {
     console.log(payload);
-    return Api().post(`upload/uploadImage`, payload);
+    return Api().post(`upload/uploadImage`, payload, configHeaders);
   },
 };
